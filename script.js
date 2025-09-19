d3c5aa4349dfa28702f97d6e709c98e672883bb2
amiljoseph03 <amiljoseph03@gmail.com>
2025-09-19 20:34:30 +0530

    updated

--- script.js
+++ script.js
@@ -7,8 +7,36 @@ const addtask=()=>{
     if (text){
         tasks.push({text:text, completed:false})
     }
-    console.log(tasks)
+    // console.log(tasks)
+
+    updatetasklist()
 }
+
+const updatetasklist=()=>{
+const tasklist= document.getElementById('task-list')
+tasklist.innerHTML=''
+
+tasks.forEach((task, index)=>{
+    const listitem =document.createElement('li')
+    listitem.innerHTML = `
+    <div class="taskitem">
+       <div class="task ${task.completed? 'completed':''}">
+        <input type="checkbox" class="checkbox" />
+        <p> finish project</p>
+        </div>
+        <div class="icons">
+       
+
+        <img src="./img/edit.png" alt="edit"/>
+        <img src="./img/bin.jpg" alt="delete"/>
+
+        </div>
+    </div>`;
+    listitem.addEventListener('change',()=>toggleTaskComplete(index))
+    tasklist.append(listitem)
+})
+}
+
 document.getElementById('newtask').addEventListener('click',function(e){
     e.preventDefault();
 
